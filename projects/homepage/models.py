e6ce655fe5ab95bf5939fccb89159da24f2a2882
Michael John <duc.nguyenminh@2359media.com>
2019-04-10 08:07:06 +0700

    Uodate:

--- projects/homepage/models.py
+++ projects/homepage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class ResultTest(models.Model):
+    age = models.DecimalField(max_digits=5, decimal_places=2)
+    sex = models.DecimalField(max_digits=5, decimal_places=2)
+    cp = models.DecimalField(max_digits=5, decimal_places=2)
+    trestbps = models.DecimalField(max_digits=5, decimal_places=2)
+    chol = models.DecimalField(max_digits=5, decimal_places=2)
+    fbs = models.DecimalField(max_digits=5, decimal_places=2)
+    restecg = models.DecimalField(max_digits=5, decimal_places=2)
+    thalach = models.DecimalField(max_digits=5, decimal_places=2)
+    exang = models.DecimalField(max_digits=5, decimal_places=2)
+    oldpeak = models.DecimalField(max_digits=5, decimal_places=2)
+    slope = models.DecimalField(max_digits=5, decimal_places=2)
+    ca = models.DecimalField(max_digits=5, decimal_places=2)
+    thai = models.DecimalField(max_digits=5, decimal_places=2)
+    num = models.DecimalField(max_digits=5, decimal_places=2)
+    
